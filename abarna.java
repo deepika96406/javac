83e9b237b8beaadefdcf60dfb9bcefac476743f4
deepika96406 <deepikajnc2002@gmail.com>
2025-04-01 14:59:19 +0530

    module2

---
+++ abarna.java
@@ -0,0 +1,27 @@
+import java.io.*;
+class abarnaSuperMarket
+{
+public Static Void Main(String args[]);
+Input Stream Reader isr=newInputStreamReader(System.in);
+BufferedReader br = newBufferedReader(isr);
+System.out.println("abarna Super Market");
+System.out.println("-----------------------");
+System.out.println("No.4,Mainroad,Villupuram");
+System.out.println("-----------------------");
+System.out.println("BIll");
+System.out.println("------------------------");
+System.out.println("Enter the serial no");
+String S1=br.readLine();
+int a = Integer.ParseInt(S1);
+System.out.println("Enter the Particulars");
+String S2=br.readLine();
+int b = Integer.ParseInt(S2);
+System.out.println("Enter the rate");
+String S3=br.readLine();
+int c = Integer.ParseInt(S3);
+System.out.println("Enter the Quantity");
+String S4=br.readLine();
+int d = Integer.ParseInt(S4);
+System.out.println("Result");
+e=a*b;
+S
